3d7f0cb1f54c6cbf91d25a16cb7066258cdee2ac
zangwang <info@zangwang.tw>
2019-11-09 20:34:10 +0800

    express套用handlebars模板，設定路由
    express路由設定要多複習

---
+++ app.js
@@ -0,0 +1,25 @@
+// require express
+const express = require('express')
+const app = express()
+// port setting
+const port = 3000
+
+// require handlebars
+const exphbs = require('express-handlebars')
+
+// handlebars semantic template
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// static files
+app.use(express.static('public'))
+
+// render page
+app.get('/', (req, res) => {
+  res.render('index', {})
+})
+
+// listen
+app.listen(port, () => {
+  console.log(`Express myself`)
+})
